4d922cd68117273d2bf002339704040971848de6
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-02 08:29:05 +0700

    Update anime-kotori.js

--- plugins/anime-kotori.js
+++ plugins/anime-kotori.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://revita.herokuapp.com/api/wallpaper/kotori?apikey=ApiRevita'
+	let url = 'https://api.zeeoneofc.xyz/api/anime/kotori-minami?apikey=YrxxzZel'
 	conn.sendButton(m.chat, 'Waifu nya om (≧ω≦)', wm, await(await fetch(url)).buffer(), [['🔁Next🔁',`.${command}`]],m)
 }
 handler.command = /^(kotori)$/i

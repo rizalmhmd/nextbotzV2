66e7bc524598c0bcf54806be11a524f3460f60c7
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-02 08:24:51 +0700

    Update anime-chitoge.js

--- plugins/anime-chitoge.js
+++ plugins/anime-chitoge.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://revita.herokuapp.com/api/wallpaper/chitoge?apikey=ApiRevita'
+	let url = 'https://api.zeeoneofc.xyz/api/anime/chitoge?apikey=YrxxzZel'
 	conn.sendButton(m.chat, 'Waifu nya om (≧ω≦)', wm, await(await fetch(url)).buffer(), [['🔁Next🔁',`.${command}`]],m)
 }
 handler.command = /^(chitoge)$/i

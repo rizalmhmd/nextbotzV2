2db247a90746914c952730ad8ab4dcf1a77b5081
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-03 08:30:20 +0700

    Update anime-elaina.js

--- plugins/anime-elaina.js
+++ plugins/anime-elaina.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command }) => {
-	let url = 'https://revita.herokuapp.com/api/wallpaper/elaina?apikey=ApiRevita'
+	let url = 'https://api.lolhuman.xyz/api/random/elaina?apikey=ee99d6df5f8c5eaf7667a90d'
 	conn.sendButton(m.chat, 'Waifu nya om (≧ω≦)', wm, await(await fetch(url)).buffer(), [['🔁Next🔁',`.${command}`]],m)
 }
 handler.command = /^(elaina)$/i

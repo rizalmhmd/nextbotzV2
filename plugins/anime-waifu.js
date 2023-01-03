9d08282e7659fdf152f0e6a6db1606702cf99569
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-03 10:51:59 +0700

    Update anime-waifu.js

--- plugins/anime-waifu.js
+++ plugins/anime-waifu.js
@@ -1,14 +1,14 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix }) => {
-    let res = await fetch('https://api.lolhuman.xyz/api/random/sfw/waifu?apikey=8e66d0934cf741bfd2182c16')
+    let res = await fetch('https://api.waifu.pics/sfw/waifu')
     if (!res.ok) throw await res.text()
     let json = await res.json()
     if (!json.url) throw 'Error!'
-    conn.sendButton(m.chat, 'Load..', author, json.url, [['waifu', `${usedPrefix}waifu`]], m)
+    conn.sendButton(m.chat, 'Istrinya Kartun', author, json.url, [['waifu', `${usedPrefix}waifu`]], m)
 }
 handler.help = ['waifu']
-handler.tags = ['internet']
+handler.tags = ['anime']
 handler.limit = true
 handler.command = /^(waifu)$/i
 //MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS

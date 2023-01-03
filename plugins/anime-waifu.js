94abffb63986d6fe56210294b84760dce58ea242
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-03 08:28:51 +0700

    Update anime-waifu.js

--- plugins/anime-waifu.js
+++ plugins/anime-waifu.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix }) => {
-    let res = await fetch('https://api.waifu.pics/sfw/waifu')
+    let res = await fetch('https://api.lolhuman.xyz/api/random/sfw/waifu?apikey=ee99d6df5f8c5eaf7667a90d')
     if (!res.ok) throw await res.text()
     let json = await res.json()
     if (!json.url) throw 'Error!'

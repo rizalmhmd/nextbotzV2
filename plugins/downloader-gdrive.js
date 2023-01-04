f7f55071b430b91b2c50bef6f9425ac440f67388
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-04 11:20:24 +0700

    Update downloader-gdrive.js

--- plugins/downloader-gdrive.js
+++ plugins/downloader-gdrive.js
@@ -14,6 +14,7 @@ let handler = async (m, { conn, args }) => {
 }
 handler.command = /^(gdrive)$/i
 handler.disabled = false
+handler.tags = ['downloader']
 handler.limit = true
 
 export default handler

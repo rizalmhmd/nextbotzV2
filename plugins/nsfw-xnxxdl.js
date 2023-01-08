6a38b1803e73311f4b93d2c1dae1d2a7cd2095b4
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-08 14:55:07 +0700

    Update nsfw-xnxxdl.js

--- plugins/nsfw-xnxxdl.js
+++ plugins/nsfw-xnxxdl.js
@@ -5,14 +5,19 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 	let user = global.db.data.users[m.sender]
 	if (user.age < 18) throw 'umur kamu belum cukup dek!';
 	try {
-		let json = await fetch(`https://malesin.xyz/xnxxdl?url=${text}`)
+		let json = await fetch(`https://api.lolhuman.xyz/api/xnxx?apikey=cahyokun&url=${text}`)
   let x = await json.json()
   let caption = `*Title:* ${x.result.title}
   *duration:* ${x.result.duration}
-  *info:* ${x.result.info}
-  *url:* ${x.result.URL}
+  *view:* ${x.result.view}
+  *rating:* ${x.result.rating}
+  *like:* ${x.result.like}
+  *dislike:* ${x.result.dislike}
+  *comment:* ${x.result.comment}
+  *tag:* ${Array.from(x.result.tag)}
+  *description:* ${x.result.description}
   `
-conn.sendFile(m.chat, x.result.files.low, 'asupan.mp4', caption, m)
+conn.sendFile(m.chat, x.result.link[1].link, 'asupan.mp4', caption, m)
 	} catch (e) {
 		throw e;
 	}

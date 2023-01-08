15362ea4ac34f9f434aeae3fe2be7c74f455a855
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-08 19:19:24 +0700

    Create nsfw-xnxxdl.js

---
+++ plugins/nsfw-xnxxdl.js
@@ -0,0 +1,33 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+	if (!text) throw `uhm.. mau download apa?\n\nContoh: ${usedPrefix + command} link xnxx`
+	let user = global.db.data.users[m.sender]
+	if (user.age < 18) throw 'umur kamu belum cukup dek!';
+	try {
+		let res = await fetch(`ttps://api.lolhuman.xyz/api/xnxx?apikey=cahyokun&url=${text}`)
+		if (res.status !== 200) throw 'Nice'
+    let w = await res.json()
+    let x = w.result
+  let caption = `*Title:* ${x.result.title}
+  *duration:* ${x.result.duration}
+  *view:* ${x.result.view}
+  *rating:* ${x.result.rating}
+  *like:* ${x.result.like}
+  *dislike:* ${x.result.dislike}
+  *comment:* ${x.result.comment}
+  *tag:* ${Array.from(x.result.tag)}
+  *description:* ${x.result.description}
+  `
+  conn.sendFile(m.chat, x.result.link[1].link, 'asupan.mp4', caption, m)
+	} catch (e) {
+		throw e;
+	}
+}
+handler.help = ["xnxxdl"].map(v => v + ' <url>')
+handler.tags = ["nsfw"]
+handler.command = /^(xnxxdl|dlxnxx)$/i
+handler.register = true
+handler.premium = true
+
+export default handler

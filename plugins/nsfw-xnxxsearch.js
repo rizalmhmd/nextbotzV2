c642b9f1d5fb1375f06f375cf21d608af4aa86a2
rizalmhmd <92542385+rizalmhmd@users.noreply.github.com>
2023-01-08 19:13:14 +0700

    Create nsfw-xnxxsearch.js

---
+++ plugins/nsfw-xnxxsearch.js
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+	if (!text) throw `uhm.. mau cari apa?\n\nContoh: ${usedPrefix + command} bokep🐒`
+	let user = global.db.data.users[m.sender]
+	if (user.age < 18) throw 'umur kamu belum cukup dek!\nawas jangan nonton beginian ya dek, nanti aku bilangin ke ibu kamu loh ><';
+	try {
+    let res = await fetch(`https://api.lolhuman.xyz/api/xnxxsearch?apikey=cahyokun&query=${text}`)
+		let rest = await res.json()
+		let cap = `Hasil Pencarian Dari ${text}\n`
+	for (let v of rest.result) {
+	 cap += `• *Title :* ${v.title}
+views: ${v.views}
+duration: ${v.duration}
+uploader: ${v.uploader}
+link: ${v.link}
+`
+cap +=  '\n' + '••••••••••••••••••••••••' + '\n'
+    }
+  m.reply(cap)
+	} catch (e) {
+		console.error(e);
+		throw e;
+	}
+}
+handler.help = ["xnxxsearch"].map(v => v + ' <query>')
+handler.tags = ["nsfw"]
+handler.command = /^xnxxsearch|xnxxs$/i
+
+handler.register = true
+handler.premium = true
+
+export default handler
